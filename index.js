2771e70672a96e4359311db476c934555e73c18a
michitatara <59904212+michitatara@users.noreply.github.com>
2020-03-02 13:13:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import FormContainer from "./containers/FormContainer";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="col-md-6">
+        <h3> BI Discussion Forum </h3>
+        <FormContainer />
+      </div>
+    );
+  }
+}
+
+console.log(document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
